2ce78e2605e71f21431836acb95600d09bcb11e2
Andrew Cline <andrew.cline@csm.cc>
2021-12-29 10:18:55 -0500

    feat: finish part 1 of day 1

--- day-1/main.go
+++ day-1/main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	dat, err := os.ReadFile("./test.txt")
+	dat, err := os.ReadFile("depths.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	fileStr := string(dat)
+	fileStr := strings.TrimSpace(string(dat))
 
-	depthStrings := strings.Split(fileStr, "/n")
+	depthStrings := strings.Split(fileStr, "\n")
 
-	depths := make([]int, len(depthStrings)-1)
+	depths := make([]int, len(depthStrings))
 	for i, ds := range depthStrings {
 		if i != 0 {
 			parsedDepth, err := strconv.Atoi(ds)
-
 			if err != nil {
 				panic(err)
 			}
 
-			depths[i-1] = parsedDepth
+			depths[i] = parsedDepth
 		}
 	}
+
+	timesDepthIncreased := 0
+
+	for i, depth := range depths {
+		if i != 0 {
+			prevDepth := depths[i-1]
+
+			if depth > prevDepth {
+				timesDepthIncreased++
+			}
+
+		}
+	}
+
+	fmt.Println(timesDepthIncreased)
 }
